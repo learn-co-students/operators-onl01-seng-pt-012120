145459edadf23e47803225977fe7205d63b78112
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-22 23:45:33 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,18 @@
-def unsafe?(speed)
 
+#require "pry"
+
+def unsafe?(speed)
+	if speed > 60 || speed < 40
+		return true
+  end
+	# default to false
+		return false
 end
 
 
 
 def not_safe?(speed)
-	
+	#binding.pry
+	# speed > 60 || speed < 40 ? true : false
+	speed.between?(40,60) ? false : true
 end
-	
-
-
