476c7ae45d8fa674c94d828ebfe811b2bef3f55f
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-02-01 02:31:57 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,19 +1,30 @@
-def unsafe?(speed)
-  if speed == unsafe?
-    "true"
-  else
-    "false"
-  end
-end
 
 def unsafe?(speed)
-  if (speed < 40 || speed > 60)
+  if speed < 40 || speed > 60
     true
-else
+  else
     false
-  end
+    end
 end
-
-def not_safe?(speed)
+  
+  
+def not_safe?(speed) 
   speed < 40 || speed > 60 ? true : false
 end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ 
